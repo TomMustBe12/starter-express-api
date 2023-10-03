07c091e7650019f8c4c6ed00c12dc6acccadc417
TomMustBe12 <122141721+TomMustBe12@users.noreply.github.com>
2023-10-03 11:13:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    res.send('Hello!! This is like really cool. My website!!')
 })
 app.listen(process.env.PORT || 3000)
